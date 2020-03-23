321eb59cf8cc38c012848bf52a6c2d3a1ac6201c
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-03-23 04:29:10 -0300

    Simple frontend. Deal with CORS

--- editor.js
+++ editor.js
@@ -1,4 +1,22 @@
 var javaEditor = CodeMirror.fromTextArea(document.getElementById("java-code"), {
     lineNumbers: true,
-    mode: "text/x-java"
+    mode: "text/x-java",
+    value: `
+package test;
+public class A {
+	public static void main(String[] args) {
+		int i = 0;
+		i++;
+	}
+}`
   });
+
+function compile() {
+    var editorContents = javaEditor.getValue();
+    fetch("http://localhost:8080/compile", {
+        method: "POST",
+        body: editorContents
+    })
+    .then(data => console.log("Received the follwoing data: " + data))
+
+}
