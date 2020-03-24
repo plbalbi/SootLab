2a68590f12f5b7648a8edf531d259fdbefe65a85
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-03-23 22:30:00 -0300

    UI in progess

--- editor.js
+++ editor.js
@@ -1,15 +1,29 @@
 var javaEditor = CodeMirror.fromTextArea(document.getElementById("java-code"), {
     lineNumbers: true,
     mode: "text/x-java",
-    value: `
-package test;
-public class A {
-	public static void main(String[] args) {
-		int i = 0;
-		i++;
-	}
-}`
+    theme: "idea"
   });
+javaEditor.setOption("extraKeys", {
+    "Cmd-Enter": cm => {
+        console.log("Called compile from Cmd+Enter");
+        compile();
+    }
+})
+javaEditor.setSize("100%", "100%")
+
+developBootstrap();
+
+function developBootstrap() {
+    javaEditor.setValue(`package test;
+    public class A {
+        public static void main(String[] args) {
+            int i = 0;
+            i++;
+        }
+    }
+    `);
+    compile();
+}
 
 function compile() {
     var editorContents = javaEditor.getValue();
